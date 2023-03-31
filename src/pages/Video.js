ff1f389a565bc3aae44487dc3d7eece154693b5b
Hellzito <waguinbk@gmail.com>
2023-03-30 23:00:45 -0300

    modified:   src/pages/Video.js

--- src/pages/Video.js
+++ src/pages/Video.js
@@ -23,7 +23,7 @@ function Video() {
         ref={videoRef}
         onClick={handleStart}
         loop
-        src="https://firebasestorage.googleapis.com/v0/b/jornada2-eb156.appspot.com/o/ZqU6oFX6.mp4.mp4?alt=media&token=9839e872-2d5e-4da3-9299-17eb2949831d"
+        src="https://privaty.com.br/wp-content/uploads/2023/03/gato-1.mp4"
       ></video>
     </div>
   );
