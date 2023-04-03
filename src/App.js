f45ac9efb2b2c61654aa0490cfbaf15632396c22
Hellzito <waguinbk@gmail.com>
2023-04-02 23:29:14 -0300

    Aula 06

--- src/App.js
+++ src/App.js
@@ -5,6 +5,12 @@ import db from "./config/firebase";
 import { collection, getDocs } from "firebase/firestore/lite";
 
 function App() {
+  let maxHeight;
+
+  if (window.innerHeight <= 800) {
+    maxHeight = window.innerHeight;
+  }
+
   const [video, setVideos] = useState([]);
 
   async function getVideos() {
@@ -19,7 +25,7 @@ function App() {
   }, []);
 
   return (
-    <div className="App">
+    <div className="App" style={{ maxHeight: maxHeight + "px" }}>
       <div className="app__videos">
         {video.map((item) => {
           return (
