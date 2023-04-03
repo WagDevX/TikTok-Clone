c6d5ab6d46061b71b110eeb534e5ce561225798d
Hellzito <waguinbk@gmail.com>
2023-04-03 02:01:43 -0300

    Nova função para pausar

--- src/pages/Video.js
+++ src/pages/Video.js
@@ -1,10 +1,11 @@
-import React, { useRef, useState } from "react";
+import React, { useRef, useState, useEffect } from "react";
 import VideoFooter from "./components/footer/VideoFooter";
 import VideoSideBar from "./components/sidebar/VideoSideBar";
 import "./video.css";
+import { debounce } from 'lodash';
 
 
-function Video({likes, messages, shares, name, description, music, url}) {
+function Video({ likes, messages, shares, name, description, music, url }) {
   const videoRef = useRef(null);
 
   const [play, setPlay] = useState(false);
@@ -19,6 +20,24 @@ function Video({likes, messages, shares, name, description, music, url}) {
     }
   }
 
+  useEffect(() => {
+    const handleScrollDebounced = debounce(() => {
+      if (videoRef.current) {
+        videoRef.current.currentTime = 0; // Define o tempo de reprodução para 0 segundos
+        videoRef.current.pause(); // Pausa a reprodução do vídeo
+        setPlay(false); // Atualiza o estado do botão de reprodução
+      }
+    }, 800);
+  
+    window.addEventListener("keydown", handleScrollDebounced);
+    window.addEventListener("wheel", handleScrollDebounced); // Adiciona o ouvinte de evento para o evento "keydown"
+  
+    return () => {
+      window.removeEventListener("keydown", handleScrollDebounced);
+      window.addEventListener("wheel", handleScrollDebounced); // Remove o ouvinte de evento quando o componente é desmontado
+    };
+  }, []);
+
   return (
     <div className="video">
       <video
@@ -28,16 +47,8 @@ function Video({likes, messages, shares, name, description, music, url}) {
         loop
         src={url}
       ></video>
-      <VideoSideBar 
-      likes={likes}
-      messages={messages}
-      shares={shares}
-      />
-      <VideoFooter 
-      name={name}
-      description={description}
-      music={music}
-      />
+      <VideoSideBar likes={likes} messages={messages} shares={shares} />
+      <VideoFooter name={name} description={description} music={music} />
     </div>
   );
 }
